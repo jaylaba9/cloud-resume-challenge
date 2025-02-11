4bb1daf9261029a026aba8cd7c10f114b6dc1cc6
Jaroslaw Laba <jarek.laba@gmail.com>
2025-02-11 22:34:34 +0100

    added test for invalid body

---
+++ tests/cypress/e2e/api_invalid_body_spec.cy.js
@@ -0,0 +1,15 @@
+import { apiUrl } from '../../../frontend/script/config.js';
+
+describe('CRC API - request with invalid body', () => {
+  it('should return 400 when invalid json is passed', () => {
+    cy.request({
+      method: 'POST',
+      url: apiUrl,
+      body: '{invalid: "json"}',
+      failOnStatusCode: false,
+    }).then((response) => {
+      expect(response.status).to.eq(400);
+      expect(response.body).to.have.property('error');
+    });
+  });
+});
