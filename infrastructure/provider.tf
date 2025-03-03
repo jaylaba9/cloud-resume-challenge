82b2d103181175eac496ce29ced778d8aea21efb
Jaroslaw Laba <jarek.laba@gmail.com>
2025-03-03 22:04:28 +0100

    testing github actions again

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -9,5 +9,5 @@ terraform {
 
 provider "aws" {
   region = "eu-central-1"
- # profile = "my-prod-profile" -> uncomment this line when using Terraform locally(not via GitHub Actions)
+ #profile = "my-prod-profile" # uncomment this line when using Terraform locally(not via GitHub Actions)
 }
