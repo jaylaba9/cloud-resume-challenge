1f210003f67decc0a197959eefdc5f225ffadb24
Jaroslaw Laba <jarek.laba@gmail.com>
2025-06-10 22:45:20 +0200

    added handler for downloading resume

--- frontend/script/script.js
+++ frontend/script/script.js
@@ -4,6 +4,38 @@ import { APIURL } from './config.js';
 const visitorCount = document.querySelector('.visitor-count');
 const downloadButton = document.querySelector('.download');
 
+// Handling download
+async function downloadResume() {
+  try {
+    const response = await fetch(
+      'https://mycrcstaticwebsite.s3.eu-central-1.amazonaws.com/public/cv.pdf'
+    );
+    if (!response.ok) {
+      throw new Error(
+        `An error occured during download: ${response.status} ${response.statusText}`
+      );
+    }
+
+    const blob = await response.blob();
+    const url = URL.createObjectURL(blob);
+
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = 'Jaroslaw_Laba_Resume.pdf';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+
+    URL.revokeObjectURL(url);
+  } catch (error) {
+    console.error(`An error occured during download: ${error}`);
+    alert('Error during downloading. Please try again in a while.');
+  }
+}
+
+downloadButton.addEventListener('click', downloadResume);
+
+// Handing data fetch
 let countValue = 0;
 
 const requestOptions = {
